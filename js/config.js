ff3cdfa19fbce08abdd0d6b2d81f151742533a58
SebsamGH <113599502+SebsamGH@users.noreply.github.com>
2024-12-20 02:01:23 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_JPtDyMZhE', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4j7faflfce54mnnagkqu8kagji', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_QJveZX1pd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '31s2b92lhihh7lv6m8jumtkqta', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
